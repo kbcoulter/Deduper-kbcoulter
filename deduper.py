c8d9108558281a79d438adb3b7446924cfc280e8
kbcoulter <coulterkb@yahoo.com>
2025-10-29 23:03:57 -0700

    Finished functions, added gitignore, started on main body

--- deduper.py
+++ deduper.py
@@ -4,7 +4,20 @@
 ### DEDUPER: Removes PCR Duplicates From Sorted SAM File ###
 ############################################################
 
+###########################
+### IMPORT AND ARGPARSE ###
+###########################
+
+import argparse
 import re
+
+def get_args():
+     parser = argparse.ArgumentParser(description="Script to remove PCR duplicates, accepting a sorted paired end SAM file and UMI file")
+     parser.add_argument("-f", "--file", help="Absolute file path for sorted input SAM file", required=True, type=str)
+     parser.add_argument("-o", "--outfile", help="Absolute file path for deduplicated SAM file", required=True, type=str)
+     parser.add_argument("-u", "--umi", help="File containing list of UMIs", required = True, type=str)
+     parser.add_argument("-h", "--help", help="HELP ME", required = False, type=str)
+     return parser.parse_args()
 
 #################
 ### FUNCTIONS ###
@@ -12,60 +25,71 @@ import re
 
 def fivepstart(POS:int, CIGAR:str, FLAG:int) -> int:
 
+    def stripchar (chunk:str) -> int:
+        return(int(re.sub('[A-Z]', '', chunk)))
 
     pattern = r'\d+[A-Z]'
-    re.findall(pattern, CIGAR)
-
-
-    # MINUS STRAND GOING 3 -> 5
-    M = # MATCH
-    D = - # DELETION
-    S = + # SOFTCLIPPING
-    N = + # SKIPPING
-
-
-    leading = True # is the leading letter
+    chunks = re.findall(pattern, CIGAR)
 
     if FLAG & ( 1 << 16): # MINUS STRAND
-        read_len = 0
-        val = "0"
-        for char in CIGAR:
-            if char.isnumeric:
-                val.join(str(char))
-            else:
-                if char == "S":
-                    if not leading:
-                        #SUBTRACT NUM FROM REFERENCE
-                        leading = False # SET TO FALSE
-                    else:
-                        # DO NOTHING
-                elif char == "M":
-                    leading = False
+        for chunk in chunks:
+            if "D" or "M" or "S" in chunk:
+                POS += stripchar(chunk)
 
-                else: # if character = D, N
-                    read_len += int(val)
+        first = chunk[-1]
+        if "S" in first:
+            POS += stripchar(first)
 
-    else: # PLUS STRAND DONE 
-        val = "0"
-        for char in CIGAR:
-            if char.isnumeric:
-                val = val.join(str(char))
-            else:
-                if char == "S" and leading:
-                    POS = POS - int(val) #SUBTRACT NUM FROM REFERENCE
-                    continue
+    else: # PLUS STRAND 
+        first = chunks[0]
+        if "S" in chunk:
+            POS -= stripchar(first) #SUBTRACT NUM FROM REFERENCE
 
-        return POS
-
+    return POS
 
 def umigrabber(QNAME:str) -> str:
     if len(QNAME) < 8:
-        return(f"ERROR w UMI: {QNAME}")
+        return(f"ERROR: UMI TOO SHORT")
     else:
         return QNAME[-8:]
+    
+###################
+### SCRIPT BODY ###
+###################
 
 def main():
-    # print(umigrabber("NS500451:154:HWKTMBGXX:1:11101:6251:1098:GTTACGTA")) # TESTING
+    args = get_args()
+    with open(outfile, 'x') as outfile:
+        with open(file, 'r') as file:
+            for line in file:
+                line = line.rstrip('\n')
+                if "@" in line[0]:
+                    outfile.write(line)
+                else:
+                    linecols = line.split('\t')
+                    UMI = linecols[UH]
+                    if UMI not in UMISET:
+                        continue
+                    else:
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
